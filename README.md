a73bdebef3ff2127f7c3f297c16a2448badf1bf9
ajaykumarsingh110@gmail.com <ajaykumarsingh110@gmail.com>
2023-09-21 16:22:51 +0530

    just before removing cdn

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/ajay-CaTi/v1.git
 git push -u origin main
+
+# Why use React
+
+React just give some helper methods to develop faster application.
+
+# Execute the app
+
+Use npx
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,29 @@
+const heading = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h2", {
+            id: "duniya"
+        }, "This is sibling 2"),
+        React.createElement("h2", {
+            id: "duniya"
+        }, "This is sibling 1")
+    ]),
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h2", {
+            id: "duniya"
+        }, "This is sibling 2"),
+        React.createElement("h2", {
+            id: "duniya"
+        }, "This is sibling 1")
+    ])
+]); // This is CORE of react
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
