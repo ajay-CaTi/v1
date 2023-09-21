d6929fc54dad229274f4df9cc12fabdac8369335
ajaykumarsingh110@gmail.com <ajaykumarsingh110@gmail.com>
2023-09-21 15:31:52 +0530

    Core react create element

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h2", { id: "duniya" }, "This is sibling 2"),
+    React.createElement("h2", { id: "duniya" }, "This is sibling 1"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h2", { id: "duniya" }, "This is sibling 2"),
+    React.createElement("h2", { id: "duniya" }, "This is sibling 1"),
+  ]),
+]); // This is CORE of react
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
